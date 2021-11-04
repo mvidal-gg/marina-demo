89aec91df0bff164a5c6fc85b5daca298f459ac1
McPixeI <torres.88.bcn@gmail.com>
2021-11-04 14:16:02 +0100

    retrieve user attributes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import {Auth} from 'aws-amplify'
+
 
 function App() {
+
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then((userData) => userData)
+      .catch(() => console.log("Not signed in"));
+  }
+
+  getUser().then(data=> console.log(data))
+
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
+      
       <AmplifySignOut />
     </div>
   );
