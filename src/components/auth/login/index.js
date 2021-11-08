b089994dd335ccd5f59fc069f1ce907352d72a30
McPixeI <torres.88.bcn@gmail.com>
2021-11-08 18:28:52 +0100

    added new consum form

--- src/components/auth/login/index.js
+++ src/components/auth/login/index.js
@@ -35,7 +35,6 @@ function Login() {
           </Form>
         )}
       </Formik>
-      <p></p>
     </>
   );
 }
--- src/components/consumptions/newConsumption.js
+++ src/components/consumptions/newConsumption.js
@@ -1,12 +1,40 @@
+import queryString from 'query-string'
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
+const initialFormValues = {
+  card_number: ""
+}
+
 export default function NewConsumption() {
-    const { search } = useLocation()
-    console.log(search)
+  const [cardNumber, setCardNumber] = useState("");
+  
+  const { search } = useLocation();
+  const values = queryString.parse(search)
+
+  useEffect(()=>{
+    setCardNumber(values.cardNumber)
+  },[])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <>
       <h3> NewConsumption component. Acceso privado</h3>
+      <Formik initialValues={initialFormValues} onSubmit={handleSubmit}>
+        {({ isSubmitting, values, isValid }) => (
+          <Form>
+            <Field type="text" name="card_number" value={cardNumber} disabled/>
+            <ErrorMessage name="card_number" component="div" />
+            <button type="submit" disabled={isSubmitting || !isValid}>
+              Continuar
+            </button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
