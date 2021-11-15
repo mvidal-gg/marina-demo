7d7424ecfa08e09dd9ed5f671d1c6b2bd56e8856
McPixeI <torres.88.bcn@gmail.com>
2021-11-15 09:58:54 +0100

    forcing deploy

--- src/components/salePoints/index.js
+++ src/components/salePoints/index.js
@@ -3,8 +3,7 @@ export default function SalePoints() {
   
   return (
     <>
-      <h3> SalePoints component. Acceso privado</h3>
-      
+      <h3> SalePoints component. Acceso privado</h3>    
     </>
   );
 }
