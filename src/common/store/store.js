013f494662806303a642b623b50b248a32fde9c0
McPixeI <torres.88.bcn@gmail.com>
2021-12-16 00:29:08 +0100

    reset store on logout

--- src/common/store/store.js
+++ src/common/store/store.js
@@ -1,14 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import consumptionsReducer from "../features/consumptions/consumptionsSlice";
 import pointsOfSaleReducer from "../features/pointsOfSale/pointsOfSaleSlice";
 
+const combinedReducer = combineReducers({
+  auth: authReducer,
+  consumptions: consumptionsReducer,
+  pointsOfSale: pointsOfSaleReducer,
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === "auth/logout/fulfilled") {
+    state = undefined;
+  }
+  return combinedReducer(state, action);
+};
+
 export default configureStore({
-  reducer: {
-    auth: authReducer,
-    consumptions: consumptionsReducer,
-    pointsOfSale: pointsOfSaleReducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
@@ -23,9 +32,10 @@ export default configureStore({
           "auth/setNewPassword/rejected",
           "consumptions/getConsumptions/pending",
           "consumptions/getConsumptions/fulfilled",
+          "consumptions/getConsumptions/rejected",
           "pointsOfSale/getPointsOfSale/pending",
           "pointsOfSale/getPointsOfSale/fulfilled",
-          "pointsOfSale/setCurrent"
+          "pointsOfSale/setCurrent",
         ],
       },
     }),
