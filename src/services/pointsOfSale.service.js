8184ccf5b957937d655cbf7a50c5fb901a81d720
Manel Vidal Prats <mvidal@grupogodo.com>
2021-12-21 12:15:58 +0100

    apiservice

--- src/services/pointsOfSale.service.js
+++ src/services/pointsOfSale.service.js
@@ -1,7 +1,9 @@
+import apiService from "./api.service";
 const endpoint =
   "https://k1c8hx53c3.execute-api.us-east-2.amazonaws.com/beta/tarjetas-club/point-of-sale?scope=internal";
 
 const getAll = (token) => {
+  console.log("endpoint3 => " + apiService.getEndpoint("POINTOFSALE"));
   return fetch(`${endpoint}`, {
     method: "GET",
     headers: {
